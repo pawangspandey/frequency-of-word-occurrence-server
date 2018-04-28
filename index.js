25e16bdbff0665d46b6fa139110a21c0bd7892dd
pawan <pawang.s.pandey@gmail.com>
2018-04-28 20:03:54 +0530

    add int conversion

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/' , (req, res) => {
 
   request.get('http://terriblytinytales.com/test.txt')
   .then((result) => {
-    res.status(200).send(util.getMostUsedWords(result, req.query.words || 5));
+    res.status(200).send(util.getMostUsedWords(result, parseInt(req.query.words) || 5));
   })
   .catch((error) => {
     // will use better way to log the error.
